9551dfff5cae3c82d3820233565693ec4f3c092e
NikolayKharkov <vtiko564@gmail.com>
2021-12-15 17:25:18 +0300

    4.2. Поиск дубликатов [#315066 #240300]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,13 +3,11 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("C:\\findDuplicates"), visitor);
-        List<Path> duplicates = visitor.getDuplicates();
-        duplicates.forEach(System.out::println);
+        visitor.getDuplicates().forEach(System.out::println);
     }
 }
