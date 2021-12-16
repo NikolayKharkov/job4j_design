a19cf39cdd7e3157c49f4dfa91b0e911cde050fe
NikolayKharkov <vtiko564@gmail.com>
2021-12-16 19:56:58 +0300

    6. Кодировка. [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -31,10 +31,10 @@ public class ConsoleChat {
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
         do {
-            if (userInput.equalsIgnoreCase(this.STOP)) {
+            if (userInput.equalsIgnoreCase(STOP)) {
                 stop = true;
             }
-            if (userInput.equalsIgnoreCase(this.CONTINUE)) {
+            if (userInput.equalsIgnoreCase(CONTINUE)) {
                 stop = false;
             }
             if (stop) {
@@ -42,21 +42,21 @@ public class ConsoleChat {
                 userInput = input.nextLine();
             }
             if (!stop) {
-                String botAnswer = botAnswers.get(0 + (int) (Math.random() * botAnswers.size()));
+                String botAnswer = botAnswers.get((int) (Math.random() * botAnswers.size()));
                 System.out.println(botAnswer);
                 log.add("Пользователь: " + userInput);
                 log.add("Бот: " + botAnswer);
                 userInput = input.nextLine();
             }
         }
-        while (!userInput.equalsIgnoreCase(this.OUT));
+        while (!userInput.equalsIgnoreCase(OUT));
         log.add("Пользователь: " + userInput);
         saveLog(log);
     }
 
     private List<String> readPhrases() {
         List<String> result = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(this.botAnswers, Charset.forName("WINDOWS-1251")))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
             result = in.lines()
                     .collect(Collectors.toList());
         } catch (Exception e) {
@@ -66,7 +66,7 @@ public class ConsoleChat {
     }
 
     private void saveLog(List<String> log) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(this.path, Charset.forName("UTF-8"), true))) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("UTF-8"), true))) {
             log.forEach(pw::println);
         } catch (Exception e) {
             e.printStackTrace();
