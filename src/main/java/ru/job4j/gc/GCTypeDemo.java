a52b543b66d3802bdf79a3b9c0c7f5ad10e4de53
NikolayKharkov <vtiko564@gmail.com>
2022-01-03 15:41:28 +0300

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/GCTypeDemo.java
+++ src/main/java/ru/job4j/gc/GCTypeDemo.java
@@ -8,9 +8,7 @@ public class GCTypeDemo {
         int length = 100;
         String[] data = new String[1_000_000];
         for (int i = 0; ; i = (i + 1) % data.length) {
-            data[i] = String.valueOf(
-                    (char) random.nextInt(255)
-            ).repeat(length);
+            data[i] = String.valueOf((char) random.nextInt(255)).repeat(length);
         }
     }
 }
