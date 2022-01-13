88271cafec70b2fdde36d8ac87fadc1cbddb04b2
NikolayKharkov <vtiko564@gmail.com>
2022-01-13 15:28:27 +0300

    2. Что такое TDD? [#4918]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,14 +1,18 @@
 package ru.job4j.tdd;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void buy() {
         Account account = new AccountCinema();
@@ -16,22 +20,34 @@ public class CinemaTest {
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertNull(ticket);
+        assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
     @Test
     public void find() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertNull(sessions);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
+    @Ignore
     @Test
-    public void notFind() {
+    public void whenNotFound() {
         Cinema cinema = new Cinema3D();
-        cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> false);
-        assertNull(sessions);
+        assertTrue(sessions.isEmpty());
+    }
+
+    @Ignore
+    @Test
+    public void whenBuyNotSuccessfully() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertNull(ticket);
     }
 }
